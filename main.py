f94e24208d0d43606101f3ab51b20d837192e986
EliasPython <157954816+EliasPython@users.noreply.github.com>
2024-03-30 09:01:07 +0100

    v1.0.2

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Imports ------
 
-# Nothing needed
+import quit
+import function3
+try:
+	import function2
+except:
+	print("fnc2: Not loaded")
 
-# Imports -------
+# Imports-end----
 
 
 # Main printing 
@@ -21,15 +26,34 @@ print("EzHack made by Elias")
 print("Discord Contact: eliasmw7")
 print("OpenSource project legal hacking tool")
 print("---------------------------------------------------------------")
+
 # Option
-print("Options: ")
-print("1: Credits")
+print("- 1 - Credits")
+print("- 2 - quit")
+print("- 0 - null") # Unused
+print("- 0 - null") # Unused
+print("- 0 - null") # Unused
+print("- 0 - null") # Unused
+print("---------------------------------------------------------------")
 
 
-
+#Others
 option = input("Option:")
+def nsc():
+    with open('function2.py', 'r') as file:
+        code = file.read()
+    exec(code)
+
+
+
 # If statements
 if option == 1:
+	function3.crd()
+if option == 2:
+	quit.quit()
+else:
+	print("?ERROR?: Inavlid option")
+
 	                                    
                                                             
                                                             
